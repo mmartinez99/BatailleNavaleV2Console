0242afd1bbfdee4861e7826c2d48323e746a9f1b
mmartinez99 <57175461+mmartinez99@users.noreply.github.com>
2022-08-10 15:03:31 +0200

    Initial commit

---
+++ ConsoleApp_BatailleNavaleV2/ConsoleApp_BatailleNavaleV2/Model/Position.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp_BatailleNavaleV2.Models
+{
+    public class Position //Définissions des coordonnées X et Y
+    {
+        public int x { get; set; } = -1;
+        public int y { get; set; } = -1;
+    }
+}
